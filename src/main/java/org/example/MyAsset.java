cc2078049bfe49e435dc12458067ddfd9687f9c2
vagrant <vagrant@vagrant.vm>
2020-11-13 07:07:11 +0000

    updates

--- src/main/java/org/example/MyAsset.java
+++ src/main/java/org/example/MyAsset.java
@@ -12,5 +12,28 @@ import org.json.JSONObject;
 @DataType()
 public class MyAsset {
 
-    
+    @Property()
+    private String value;
+
+    public MyAsset(){
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String toJSONString() {
+        return new JSONObject(this).toString();
+    }
+
+    public static MyAsset fromJSONString(String json) {
+        String value = new JSONObject(json).getString("value");
+        MyAsset asset = new MyAsset();
+        asset.setValue(value);
+        return asset;
+    }
 }
