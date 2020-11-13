426c46a7f61cc32daf6be669049c30a0af11767a
arjunachari12 <68580129+arjunachari12@users.noreply.github.com>
2020-11-13 09:10:42 +0530

    Add files via upload

---
+++ src/main/java/org/example/MyAsset.java
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.example;
+
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+import org.json.JSONObject;
+
+@DataType()
+public class MyAsset {
+
+    
+}
---
+++ src/main/java/org/example/MyAssetContract.java
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.example;
+
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.Default;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
